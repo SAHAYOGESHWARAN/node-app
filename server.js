72cc48e4b37bd1ec884885904b35abb0b72be0cc
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-07-22 09:08:54 +0530

    add node app master

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-const{ header,url,method } = req
-    console.log(header,url,method);
-    res.end();
-});
+const todos =[
+    {id: 1, text: 'Todo One'},
+    {id: 2, text: 'Todo two'},
+    {id: 2, text: 'Todo three'},
+]
 
+
+const server = http.createServer((req, res) => {
+    res.setHeader('content-type','application/json');
+    res.setHeader('x-Powered-by','node.js')
+    res.end(
+        json.stringify({
+    success: true,
+    data: todos
+}));
+});
 const PORT = 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
